285f22d251a336498a5c1d4c1a85cc0c03d3ad3f
SantitoSB <santosb@mail.ru>
2021-01-15 16:32:12 +0300

    Search by relations. Multitenantable. Divide by user view

---
+++ l8/database/migrations/2021_01_15_125653_add_created_by_user_id_to_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreatedByUserIdToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedInteger('created_by_user_id');
+            $table->foreign('created_by_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
